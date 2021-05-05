3672a5d10bc2bb910bc60382589096965e74b725
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-05 10:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,4 +27,21 @@ class Triangle extends Polygon{
   }
 }
 
+class Square extends Polygon {
+
+  get isValid() {
+    let x = this.sides[0];
+    let y = this.sides[1];
+    let z = this.sides[2];
+    let w = this.sides[3];
+    return (x === y && y === z && z === w)? true : false;
+  }
+
+  get area() {
+    let x = this.sides[0];
+    return (x * x);
+  }
+}
+
+
 
